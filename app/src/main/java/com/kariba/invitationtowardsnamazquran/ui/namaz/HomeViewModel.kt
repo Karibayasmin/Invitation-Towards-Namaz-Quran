01c6d386d98d6d9d0d06a468873ac7d7ff94d3b2
@Kariba <karibayasmin@gmail.com>
2021-07-13 12:41:04 +0600

    Modified HomeViewModel

--- app/src/main/java/com/kariba/invitationtowardsnamazquran/ui/namaz/HomeViewModel.kt
+++ app/src/main/java/com/kariba/invitationtowardsnamazquran/ui/namaz/HomeViewModel.kt
@@ -1,13 +1,32 @@
 package com.kariba.invitationtowardsnamazquran.ui.namaz
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kariba.invitationtowardsnamazquran.models.SuraOrDuaItem
 
 class HomeViewModel : ViewModel() {
+    var mutableSuraAndDoaListData = MutableLiveData<ArrayList<SuraOrDuaItem>>()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun loadSuraAndDoaList(context: Context) : LiveData<ArrayList<SuraOrDuaItem>> {
+        mutableSuraAndDoaListData = MutableLiveData()
+        getMutableSuraAndDoaData(context)
+        return mutableSuraAndDoaListData
     }
-    val text: LiveData<String> = _text
+
+    private fun getMutableSuraAndDoaData(context: Context) {
+        var suraAndDoaList : ArrayList<SuraOrDuaItem> = ArrayList()
+
+        suraAndDoaList.add(SuraOrDuaItem("1","Sura Al Fateha"))
+        suraAndDoaList.add(SuraOrDuaItem("2","Sura Ekhlas"))
+        suraAndDoaList.add(SuraOrDuaItem("3","Sura Falaq"))
+        suraAndDoaList.add(SuraOrDuaItem("4","Sura Nas"))
+
+        setSuraAndDoaListData(suraAndDoaList)
+    }
+
+    private fun setSuraAndDoaListData(suraAndDoaList: java.util.ArrayList<SuraOrDuaItem>) {
+        mutableSuraAndDoaListData.value = suraAndDoaList
+    }
 }
